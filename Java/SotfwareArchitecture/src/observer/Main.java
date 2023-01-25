1aa86fce0cf687da8c3ac5f9b65d476dc70bd72a
ljk <away0419@naver.com>
2023-01-25 17:50:00 +0900

    디자인패턴

---
+++ Java/SotfwareArchitecture/src/observer/Main.java
@@ -0,0 +1,19 @@
+package observer;
+
+public class Main {
+	public static void main(String[] args) {
+		Notice notice = new Notice();
+		User1 user1 = new User1("유저1");
+		User2 user2 = new User2("유저2");
+		
+		notice.attach(user1);
+		notice.attach(user2);
+		
+		String msg ="공지!!!";
+		notice.notifyObservers(msg);
+		
+		notice.detach(user1);
+		msg = "안녕하세요~";
+		notice.notifyObservers(msg);
+	}
+}
