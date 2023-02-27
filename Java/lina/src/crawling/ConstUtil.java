f406efae2dfc1708dca74475c4fdd882ccdf9354
away0419 <away0419@naver.com>
2023-02-27 16:44:15 +0900

    크롤링

---
+++ Java/lina/src/crawling/ConstUtil.java
@@ -0,0 +1,9 @@
+package crawling;
+
+public interface ConstUtil {
+	String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    String WEB_DRIVER_PATH = "C:\\Users\\KTDS\\Downloads\\chromedriver_win32\\chromedriver.exe";
+    String NOTICE_BASE_URL = "https://www.lina.co.kr/cms/customer/notice/index,1,list1,";
+	int NOTICE_NUM_PAGES = 56;
+	String NOTICE_ONE_URL = "https://www.lina.co.kr/cms/customer/notice/index.htm";
+}
---
+++ Java/lina/src/crawling/Main.java
@@ -0,0 +1,29 @@
+package crawling;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import custom.excel.ExcelDTO;
+import custom.excel.ExcelFunction;
+
+public class Main {
+	public static void main(String[] args) {
+		System.setProperty(ConstUtil.WEB_DRIVER_ID, ConstUtil.WEB_DRIVER_PATH);
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		WebDriver driver = new ChromeDriver(options);
+
+		
+		Crawling crawling = new Crawling();
+		List<ExcelDTO> list = crawling.makeNoticeList(driver);
+
+
+		ExcelFunction excelFunction = new ExcelFunction();
+		excelFunction.makeExcel("notice", list);
+
+		driver.quit();
+	}
+}
