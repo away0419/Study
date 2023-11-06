19e8b61423799284d02ebb4f6e44fff94339d4c2
LJK <away0419@naver.com>
2023-11-06 17:52:56 +0900

    :hammer: JWTProvider 커스텀 예외로 변경, Member 필드 추가에 따른 변경

--- kotlin/src/main/kotlin/com/example/kotlin/security/oauth2/OAuth2Attributes.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/security/oauth2/OAuth2Attributes.kt
@@ -1,51 +1,70 @@
 package com.example.kotlin.security.oauth2
 
+import com.example.kotlin.security.exception.SecurityCustomErrorCode
+import com.example.kotlin.security.exception.SecurityCustomException
+
 enum class OAuth2Attributes(
-    val nameAttributeKey: String?,
-    val oauth2UserInfo: (Map<String, Any>?) -> Oauth2UserInfo
+    val nameAttributeKey: String,
+    val oauth2UserInfo: (Map<String, Any>) -> OAuth2UserInfo
 ) {
     GOOGLE("google", { map ->
-        Oauth2UserInfo(
-            map?.get("sub").toString(),
-            map?.get("name").toString(),
-            map?.get("email").toString(),
-            map?.get("picture").toString()
+        OAuth2UserInfo(
+            map["name"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_NAME_IS_NULL),
+            map["email"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_EMAIL_IS_NULL),
+            map["picture"]?.toString(),
+            map["sub"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_KEY_IS_NULL),
+            "google"
         )
     }),
 
     NAVER("naver", { attributes ->
-        val content = attributes?.get("response");
-        val map: Map<String, Any>? = if (content is Map<*, *>) {
+        val content =
+            attributes["response"] ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_CONTENT_IS_NOT_FOUND)
+        val map: Map<String, Any> =
             content as? Map<String, Any>
-        } else {
-            emptyMap()
-        }
-        Oauth2UserInfo(
-            map?.get("id").toString(),
-            map?.get("nickname").toString(),
-            map?.get("email").toString(),
-            map?.get("profile_image").toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_CONTENT_TYPE_MISMATCH)
+        OAuth2UserInfo(
+            map["nickname"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_NAME_IS_NULL),
+            map["email"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_EMAIL_IS_NULL),
+            map["profile_image"]?.toString(),
+            map["id"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_KEY_IS_NULL),
+            "naver"
         )
     }),
 
     KAKAO("kakao", { attributes ->
-        val content = attributes?.get("properties");
-        val map: Map<String, Any>? = if (content is Map<*, *>) {
-            content as? Map<String, Any>
-        } else {
-            emptyMap()
-        }
-        Oauth2UserInfo(
-            map?.get("sub").toString(),
-            map?.get("name").toString(),
-            map?.get("account_email").toString(),
-            map?.get("thumbnail_image").toString()
+        val properties = attributes["properties"]
+            ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_CONTENT_IS_NOT_FOUND)
+        val kakao_account = attributes["kakao_account"]
+            ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_CONTENT_IS_NOT_FOUND)
+        val map: Map<String, Any> =
+            properties as? Map<String, Any>
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_CONTENT_TYPE_MISMATCH)
+        val map2: Map<String, Any> =
+            kakao_account as? Map<String, Any>
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_CONTENT_TYPE_MISMATCH)
+        OAuth2UserInfo(
+            map["nickname"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_NAME_IS_NULL),
+            map2["email"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_EMAIL_IS_NULL),
+            map["profile_image"]?.toString(),
+            attributes["id"]?.toString()
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_USER_INFO_KEY_IS_NULL),
+            "kakao"
         )
     });
 
     companion object {
-        fun extract(registrationId: String, attributes: Map<String, Any>?): Oauth2UserInfo? {
+        fun extract(registrationId: String, attributes: Map<String, Any>): OAuth2UserInfo {
             return values().find { it.nameAttributeKey == registrationId }?.oauth2UserInfo?.invoke(attributes)
+                ?: throw SecurityCustomException(SecurityCustomErrorCode.OAUTH2_SERVICE_IS_NOT_FOUND)
         }
     }
 }
