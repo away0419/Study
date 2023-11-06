096af81bb5bf681ad03d0af2e0e236e666ee18f1
LJK <away0419@naver.com>
2023-11-06 17:50:59 +0900

    :hammer: JWTProvider 커스텀 예외로 변경, 기능 설명 추가

--- kotlin/src/main/kotlin/com/example/kotlin/security/jwt/provider/JWTProvider.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/security/jwt/provider/JWTProvider.kt
@@ -1,17 +1,15 @@
 package com.example.kotlin.security.jwt.provider
 
+import com.example.kotlin.common.logger
 import com.example.kotlin.security.AuthConstants
+import com.example.kotlin.security.exception.SecurityCustomErrorCode
+import com.example.kotlin.security.exception.SecurityCustomException
 import com.example.kotlin.security.jwt.MemberPrincipal
-import io.jsonwebtoken.Claims
-import io.jsonwebtoken.ExpiredJwtException
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.security.Keys
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.*
 import jakarta.servlet.http.Cookie
 import jakarta.xml.bind.DatatypeConverter
-import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseCookie
-import org.springframework.security.oauth2.jwt.JwtException
 import org.springframework.stereotype.Component
 import java.security.Key
 import java.util.*
@@ -19,9 +17,15 @@ import javax.crypto.spec.SecretKeySpec
 
 @Component
 class JWTProvider {
-    private val jwtSecretKey = "exampleSecretKeyExampleSecretKeyExampleSecretKeyExampleSecretKey"
-    private val log = LoggerFactory.getLogger(this.javaClass)!!
+    //    @Value("\${jwt.secret}")
+    private val jwtSecretKey: String = "exampleSecretKeyExampleSecretKeyExampleSecretKeyExampleSecretKey"
+    private val log = logger()
+    private val objectMapper = ObjectMapper()
 
+    /**
+     * token 헤더 만들기
+     * @return Map<String, Any>
+     */
     private fun createHeader(): Map<String, Any> {
         val header: MutableMap<String, Any> = HashMap()
         header["typ"] = "JWT" // 토큰 타입
@@ -29,25 +33,43 @@ class JWTProvider {
         return header
     }
 
+    /**
+     * token 공개, 비공개 claims 만들기
+     * @param memberPrincipal MemberPrincipal
+     * @return Map<String, Any>
+     */
     private fun createClaims(memberPrincipal: MemberPrincipal): Map<String, Any> {
         val claims: MutableMap<String, Any> = HashMap()
         claims["email"] = memberPrincipal.email as Any
         claims["role"] = memberPrincipal.role as Any
-        claims["https://github.com/away0419/spring-security/tree/main/springBoot"] = true
+        claims["memberPrincipal"] = objectMapper.writeValueAsString(memberPrincipal) // 객체를 json 형식으로 만들기
         return claims
     }
 
+    /**
+     * token 사인 만들기
+     * @return Key
+     */
     private fun createSignature(): Key {
         val apiKeySecretBytes = DatatypeConverter.parseBase64Binary(jwtSecretKey)
         return SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.jcaName)
     }
 
+    /**
+     * access token 유효 기간 설정
+     * @return Date
+     */
     private fun createExpiredDate(): Date {
         val c = Calendar.getInstance()
         c.add(Calendar.MINUTE, 30) // 30분
         return c.time
     }
 
+    /**
+     * access token 생성
+     * @param memberPrincipal MemberPrincipal
+     * @return String
+     */
     fun generateJwtToken(memberPrincipal: MemberPrincipal): String {
         return Jwts.builder()
             .setHeader(createHeader())
@@ -59,53 +81,89 @@ class JWTProvider {
             .compact()
     }
 
+    /**
+     * 헤더에서 token 추출
+     * @param header String
+     * @return String
+     */
     fun getTokenFromHeader(header: String): String {
+        // 만약 개발자가 지정한 토큰 타입이 아닌 경우 에러 발생
         if (!header.startsWith(AuthConstants.TOKEN_TYPE)) {
-            throw Exception("${AuthConstants.TOKEN_TYPE} is not match")
+            throw SecurityCustomException(SecurityCustomErrorCode.JWT_TOKEN_TYPE_MISMATCH)
         }
         return header.split(" ")[1]
     }
 
-    fun isValidToken(token: String): Boolean {
+    /**
+     * token claims 추출
+     * @param token String
+     * @return Claims
+     */
+    fun getClaimsFromToken(token: String): Claims {
+
         return try {
-            val claims = getClaimsFromToken(token)
-            log.info("email : {}", claims["email"])
-            log.info("role : {}", claims["role"])
-            log.info("토큰 발급자 : {}", claims.subject)
-            log.info("토큰 만료 시간 : {}", claims.expiration)
-            log.info("토큰 발급 시간 : {}", claims.issuedAt)
-            true
+            Jwts.parserBuilder()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(jwtSecretKey))
+                .build()
+                .parseClaimsJws(token)
+                .body
+        } catch (e: MalformedJwtException) {
+            throw SecurityCustomException(SecurityCustomErrorCode.JWT_TOKEN_MALFORMED)
         } catch (e: ExpiredJwtException) {
-            throw Exception("Token Expired")
-        } catch (e: JwtException) {
-            throw Exception("Token Tampered or Invalid")
+            throw SecurityCustomException(SecurityCustomErrorCode.JWT_TOKEN_EXPIRED)
+        } catch (e: UnsupportedJwtException) {
+            throw SecurityCustomException(SecurityCustomErrorCode.JWT_TAMPERED_INVALID)
+        } catch (e: IllegalArgumentException) {
+            throw SecurityCustomException(SecurityCustomErrorCode.JWT_TOKEN_ILLEGAL_ARGUMENT)
         } catch (e: NullPointerException) {
-            throw Exception("Token is null")
+            throw SecurityCustomException(SecurityCustomErrorCode.JWT_TOKEN_IS_NULL)
         }
     }
 
-    private fun getClaimsFromToken(token: String): Claims {
-        return Jwts.parserBuilder()
-            .setSigningKey(DatatypeConverter.parseBase64Binary(jwtSecretKey))
-            .build()
-            .parseClaimsJws(token)
-            .body
-    }
-
+    /**
+     * token에서 email 추출
+     * @param token String
+     * @return String
+     */
     fun getUserEmailFromToken(token: String): String {
         return getClaimsFromToken(token)["email"].toString()
     }
 
+    /**
+     * token에서 role 추출
+     * @param token String
+     * @return String
+     */
     fun getUserRoleFromToken(token: String): String {
         return getClaimsFromToken(token)["role"].toString()
     }
 
+    /**
+     * token에서 memberPrincipal 추출
+     * @param token String
+     * @return MemberPrincipal
+     */
+    fun getMemberPrincipalFromToken(token: String): MemberPrincipal {
+        val memberPrincipalInfo = getClaimsFromToken(token)["memberPrincipal"] ?: throw SecurityCustomException(
+            SecurityCustomErrorCode.OAUTH2_USER_INFO_IS_NULL
+        )
+        return objectMapper.readValue(memberPrincipalInfo.toString(), MemberPrincipal::class.java)
+    }
+
+    /**
+     * refresh token 유효 기간 설정
+     * @return Date
+     */
     private fun createRefreshTokenExpiredDate(): Date {
         val c = Calendar.getInstance()
         c.add(Calendar.MONTH, 1) // 1개월
         return c.time
     }
 
+    /**
+     * refresh token 생성
+     * @return String
+     */
     fun generateRefreshToken(): String {
         return Jwts.builder()
             .setHeader(createHeader())
@@ -115,6 +173,11 @@ class JWTProvider {
             .compact()
     }
 
+    /**
+     * cookie 생성
+     * @param refreshToken String
+     * @return ResponseCookie
+     */
     fun generateRefreshTokenCookie(refreshToken: String): ResponseCookie {
         return ResponseCookie.from(AuthConstants.REFRESH_TOKEN_PREFIX, refreshToken)
             .httpOnly(true)
@@ -124,10 +187,21 @@ class JWTProvider {
             .build()
     }
 
-    fun getRefreshToken(cookies: Array<Cookie>): String? {
+    /**
+     * cookie에서 refresh token 추출
+     * @param cookies Array<Cookie>
+     * @return String
+     */
+    fun getRefreshToken(cookies: Array<Cookie>): String {
         return cookies.firstOrNull { it.name == AuthConstants.REFRESH_TOKEN_PREFIX }?.value
+            ?: throw SecurityCustomException(SecurityCustomErrorCode.JWT_COOKIE_IS_NOT_FOUND)
     }
 
+    /**
+     * refresh token 남은 유효 기간이 7일 이하 여부 판단.
+     * @param refreshToken String
+     * @return Boolean
+     */
     fun isNeedToUpdateRefreshToken(refreshToken: String): Boolean {
         val expiresAt = getClaimsFromToken(refreshToken).expiration
         val calendar = Calendar.getInstance()
