eae0f87fb460ddefd3049963cb329a254d051159
LJK <away0419@naver.com>
2023-11-06 17:46:41 +0900

    :hammer: MemberController 수정

--- kotlin/src/main/kotlin/com/example/kotlin/member/controller/MemberController.kt
+++ kotlin/src/main/kotlin/com/example/kotlin/member/controller/MemberController.kt
@@ -1,19 +1,12 @@
 package com.example.kotlin.member.controller
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/member")
 class MemberController {
-
-    @GetMapping("/login/success")
-    fun login() : String?{
-        return "login success"
-    }
-
     @GetMapping("/space")
     fun getSpace() : String?{
         return "space"
