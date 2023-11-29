5dd17ef6a9a209b604c14f8f09a34232aa117eb4
LJK <away0419@naver.com>
2023-11-29 15:22:43 +0900

    :sparkles: service test 추가

--- [23.11]TDD/kotlin/src/test/kotlin/com/example/kotlin/domain/member/service/MemberServiceTest.kt
+++ [23.11]TDD/kotlin/src/test/kotlin/com/example/kotlin/domain/member/service/MemberServiceTest.kt
@@ -12,6 +12,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import org.hibernate.annotations.processing.SQL
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.TestPropertySource
 import java.util.UUID
 
@@ -41,6 +42,11 @@ internal class MemberServiceTest: DescribeSpec({
 
             result?.shouldBeNull()
         }
+        // DB에 연결하지 않았음에도 통과하는 이유는 다음과 같다.
+        // spyk 이므로 실제 기능을 가져온다. 이때, 실제 기능은 DB가 연결 되어 있어야만 동작한다.
+        // DB가 연결 되지 않은 현재, 실제 기능이 없는 null이 되어버린다.
+        // 즉, stubbing 하지 않고 memberService.findByid()를 한다면 memberRepository 가 null이므로 null을 반환한다.
+        // 현재는 service 자체만 확인하면 되기 때문에 굳이 db를 연결할 필요없다. 어차피 순서상 repository test를 먼저하기 때문이다.
 
         it("멤버가 있다면 찾은 멤버 반환"){
             val request = UUID.fromString("4a9e5e6b-0b68-4eaa-9e38-53590a0332d4")
