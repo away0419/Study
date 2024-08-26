2c9e63363c5d8450748b986f2a0160789e8fbcd3
LJK <away0419@naver.com>
2024-08-26 21:56:30 +0900

    :sparkles: beak

---
+++ [23.01]Algorithm/baek/src/random/B10978_2.java
@@ -0,0 +1,19 @@
+package random;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10978_2 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-->0){
+            int N = Integer.parseInt(br.readLine());
+
+
+
+        }
+    }
+}
